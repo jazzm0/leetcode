853dfb922cd9e035903af34449980ad6f238b9f8
d050150 <tibor.tarnai@sap.com>
2023-11-23 15:50:13 +0100

    game of life

---
+++ game_of_life.py
@@ -0,0 +1,71 @@
+import unittest
+from typing import List
+
+
+# https://leetcode.com/problems/game-of-life
+
+class Solution:
+
+    def getLiveNeighbours(self, board: List[List[int]], i: int, j: int) -> int:
+        neighbours = 0
+        m, n = len(board), len(board[0])
+
+        if i > 0 and j > 0 and board[i - 1][j - 1] == 1:
+            neighbours += 1
+        if i > 0 and board[i - 1][j] == 1:
+            neighbours += 1
+        if i > 0 and j < n - 1 and board[i - 1][j + 1] == 1:
+            neighbours += 1
+        if j > 0 and board[i][j - 1] == 1:
+            neighbours += 1
+        if j < n - 1 and board[i][j + 1] == 1:
+            neighbours += 1
+        if i < m - 1 and j > 0 and board[i + 1][j - 1] == 1:
+            neighbours += 1
+        if i < m - 1 and board[i + 1][j] == 1:
+            neighbours += 1
+        if i < m - 1 and j < n - 1 and board[i + 1][j + 1] == 1:
+            neighbours += 1
+        return neighbours
+
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        next_state = {}
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                neighbours = self.getLiveNeighbours(board, r, c)
+                if neighbours < 2 or neighbours > 3:
+                    next_state[(r, c)] = 0
+                elif neighbours == 2:
+                    next_state[(r, c)] = board[r][c]
+                elif neighbours == 3:
+                    if board[r][c] == 0:
+                        next_state[(r, c)] = 1
+                    else:
+                        next_state[(r, c)] = board[r][c]
+
+        for k, v in next_state.items():
+            board[k[0]][k[1]] = v
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_neighbours(self):
+        m = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+
+        self.assertEqual(1, Solution().getLiveNeighbours(m, 0, 0))
+        self.assertEqual(2, Solution().getLiveNeighbours(m, 0, 2))
+        self.assertEqual(5, Solution().getLiveNeighbours(m, 1, 1))
+        self.assertEqual(3, Solution().getLiveNeighbours(m, 2, 1))
+
+    def test_a(self):
+        m = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+        Solution().gameOfLife(m)
+        self.assertEqual([[0, 0, 0], [1, 0, 1], [0, 1, 1], [0, 1, 0]], m)
+
+    def test_b(self):
+        m = [[1, 1], [1, 0]]
+        Solution().gameOfLife(m)
+        self.assertEqual([[1, 1], [1, 1]], m)
+
+
+if __name__ == "__main__":
+    unittest.main()
